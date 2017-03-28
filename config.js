2c2819d4899cdf519d56d86da48dff3da1ebb6c5
Justin Essert <justinkessert@gmail.com>
2017-03-28 15:42:19 -0500

    Removed unneeded files, added a gitignore file to allow configuration with sensitive data

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 //Defining configuration variables
 module.exports = {
 	'port':			process.env.PORT || 8080,
-	'db': 'mongodb://GoogleCloudPlatform-Public:8e1nixu9XD6g@ds135790.mlab.com:35790/justinessert-mean',
-	'adminPrefix': 'mongodb://GoogleCloudPlatform-JustinEssert:',
-	'adminSuffix': '@ds135790.mlab.com:35790/justinessert-mean',
+	'db': 'mongodb://GoogleCloudPlatform-Public:8e1nixu9XD6g@ds135790.mlab.com:35790/justinessert-mean', //example connection
 	'key': '2e0Ogc360817DcR'
 };
