2cb4811fcdc6267fb7a29852dc24991aa43500b6
Justin Essert <justinkessert@gmail.com>
2017-03-28 15:55:33 -0500

    Fixed gitignore to include all config files

--- config.js
+++
@@ -1,6 +0,0 @@
-//Defining configuration variables
-module.exports = {
-	'port':			process.env.PORT || 8080,
-	'db': 'mongodb://GoogleCloudPlatform-Public:8e1nixu9XD6g@ds135790.mlab.com:35790/justinessert-mean', //example connection
-	'key': '2e0Ogc360817DcR'
-};
